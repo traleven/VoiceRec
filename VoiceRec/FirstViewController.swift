eeada0dd9aac26ca5be707b78cefc120afeb5d2b
ivand <ivan.dolgushin@lucidrealitylabs.com>
2019-02-28 20:06:12 +0800

    Initial Commit

---
+++ VoiceRec/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  VoiceRec
+//
+//  Created by Ivan Dolgushin on 28.02.19.
+//  Copyright © 2019 traleven. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ VoiceRec/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  VoiceRec
+//
+//  Created by Ivan Dolgushin on 28.02.19.
+//  Copyright © 2019 traleven. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
