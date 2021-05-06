be0fa64d87e57bf5a74821eadeb5fb7f2c59cc25
ivand <ivan.dolgushin@lucidrealitylabs.com>
2021-05-06 10:42:31 +0300

    Initial Commit

---
+++ noodles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noodles
+//
+//  Created by Ivan Dolgushin on 06.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
